bd6154db3eee786f3cb3615049509d69990ae6c6
haytoche <haytham.tahere@student.ehb.be>
2024-06-05 11:11:14 +0100

    functie: voltooid/delete
    een functie toegevoegd wanneer een taak voltooid is of wanneer de gebruiker een taak wil verwijderen

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const taskForm = document.getElementById('task-form');
     const taskInput = document.getElementById('task-input');
-    const tasksList = document.getElementById('todo-list');
+    const tasksList = document.getElementById('tasks');
 
     taskForm.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -10,14 +10,31 @@ document.addEventListener('DOMContentLoaded', () => {
         
         if (taskText !== '') {
             addTask(taskText);
-            taskInput.value = ''; // Clear the input field
+            taskInput.value = ''; //leeg maken van de input
         }
     });
 
     function addTask(taskText) {
         const taskItem = document.createElement('li');
-        taskItem.textContent = taskText;
+
+        const taskSpan = document.createElement('span');
+        taskSpan.textContent = taskText;
+        taskSpan.addEventListener('click', () => {//functie wanneer een taak voltooid is
+            tasksList.classList.toggle('completed');//door op de taak zelf te drukken wordt deze doorgestreept
+        });
+
+        const deleteButton = document.createElement('button'); //button om een taak te verwijderen
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => {
+            tasksList.removeChild(taskItem);
+        });
+
+        taskItem.appendChild(taskSpan);
+        taskItem.appendChild(deleteButton);
         tasksList.appendChild(taskItem);
+
     }
 });
+
+
 
